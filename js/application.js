b0c405dc32b9d34125426551a88b9024d5c59ff4
Ryan Montgomery <rmontgomery429@gmail.com>
2011-02-22 18:24:31 -0500

    * implemented the seelctors for creating a project * added jquery template for project list items * added persistence of projects * added changing of input and output folder

--- js/application.js
+++ js/application.js
@@ -1,49 +1,103 @@
+Array.prototype.last = function() {return this[this.length-1];}
+
+var projects = new Lawnchair({adaptor: 'air', table: "projects"});
+
 $(document).ready(function() {
+  listProjects();
+  
   var target = document.getElementById('watchtarget');
   target.addEventListener("dragenter", dragEnterHandler);
   target.addEventListener("dragover", dragOverHandler);
   target.addEventListener("drop", dropHandler);
   
-  var browse = $('.browse').click(function(){
-    var directory = air.File.documentsDirectory;
-
-    try
-    {
-        directory.browseForDirectory("Select Directory");
-        directory.addEventListener(air.Event.SELECT, directorySelected);
-    }
-    catch (error)
-    {
-        air.trace("Failed:", error.message)
-    }
-
-    function directorySelected(event) 
-    {
-        directory = event.target;
-        var files = directory.getDirectoryListing();
-        for(var i = 0; i < files.length; i++)
-        {
-            air.trace(files[i].name);
-        }
-    }
-  });
+  $('.option.add').click(createProjectBySelectingDirectory);
   
-  // var target = document.getElementById('sass-target');
-  // target.addEventListener("dragenter", dragEnterOverHandler);
-  // target.addEventListener("dragover", dragEnterOverHandler);
-  // target.addEventListener("drop", sassDropHandler);
+  $('#nuke').click(function(){
+    projects.nuke();
+    listProjects();
+  });
 });
 
-// function dragStartHandler(event){         
-//   event.dataTransfer.effectAllowed = "copy"; 
-// } 
-// 
-// function dragEndHandler(event){ 
-//   //air.trace(event.type + ": " + event.dataTransfer.dropEffect); 
-// } 
-// 
+function listProjects(){
+  $('.projects').empty();
+  projects.all(function(project){
+    if(project){
+      appendProjectToProjectsList(project);
+    }
+  });
+  $('.select.input').click(selectInputBySelectingDirectory);
+  $('.select.output').click(selectOutputBySelectingDirectory);
+  $('.project').click(function(){
+    $(this).children('.config').toggle();
+  });
+}
 
+function selectOutputBySelectingDirectory() {
+  key = $(this).parents('.project:first').attr('data-key');
+  config = $(this).siblings('span');
+  browseDirectories(function(event){
+    projects.get(key, function(project) {
+      project.outputPath = event.target.nativePath;
+      projects.save(project);
+    });
+    //$('.input-path').html(event.target.nativePath);
+    config.html(event.target.nativePath); //.parents('.config').attr('class'))
+    //air.trace($(this).parents('.config:first').html());
+    //$(this).parents('.config:first').children('.output-path')
+    //$(this).parents('.project:first').children('.output-path').html(event.target.nativePath);
+  });
+  return false;
+}
 
+function selectInputBySelectingDirectory() {
+  key = $(this).parents('.project:first').attr('data-key');
+  config = $(this).siblings('span');
+  browseDirectories(function(event){
+    projects.get(key, function(project) {
+      project.inputPath = event.target.nativePath;
+      projects.save(project);
+    });
+    config.html(event.target.nativePath);
+    //air.trace($(this).parents('.config:first').html());
+    //$('.input-path').html(event.target.nativePath);
+    //$(this).parents('.config:first').children('.input-path').html(event.target.nativePath);
+  });
+  return false;
+}
+
+function createProjectBySelectingDirectory() {
+  browseDirectories(createProjectFromFolder);
+}
+
+function browseDirectories(callback) {
+  var directory = air.File.documentsDirectory;
+  try
+  {
+    directory.browseForDirectory("Select Directory");
+    directory.addEventListener(air.Event.SELECT, callback);
+  }
+  catch (error)
+  {
+    air.trace("Failed:", error.message)
+  }
+}
+
+function createProjectFromFolder(event) {
+  createProject(event.target.nativePath.replace(/\/$/, '').split('/').last(), event.target.nativePath, "");
+}
+
+function createProject(name, inputPath, outputPath) {
+  projects.save({
+    name: name,
+    inputPath: inputPath,
+    outputPath: outputPath
+  });
+  listProjects();
+}
+
+function appendProjectToProjectsList(project) {
+  $.tmpl($("#project-template"), project).appendTo(".projects");
+}
 
 function dragEnterHandler(event) {
   event.preventDefault();
@@ -55,7 +109,7 @@ function dragOverHandler(event){
 
 function dropHandler(event){ 
   event.preventDefault();
-  $('#watchtarget').append('<p>'+event.dataTransfer.getData("text/uri-list")+'</p>');
+  createProject(event.dataTransfer.getData("text/uri-list").replace(/\/$/, '').split('/').last(), "", "");
 }
 
 // function sassDropHandler(event) { 
@@ -65,7 +119,7 @@ function dropHandler(event){
 // }
 
 function watchDropHandler(event) {
-    $('.projects_container').removeClass('hover');
+    //$('.projects_container').removeClass('hover');
 // for(var prop in event){ 
 //   air.trace(prop + " = " + event[prop]);
 // }
@@ -117,46 +171,46 @@ function watchDropHandler(event) {
 
 function onOutputData()
 {
-  air.trace("Got: ", process.standardOutput.readUTFBytes(process.standardOutput.bytesAvailable)); 
+  //air.trace("Got: ", process.standardOutput.readUTFBytes(process.standardOutput.bytesAvailable)); 
 }
 
 function onErrorData(event)
 {
-  air.trace("ERROR -", process.standardError.readUTFBytes(process.standardError.bytesAvailable)); 
+  //air.trace("ERROR -", process.standardError.readUTFBytes(process.standardError.bytesAvailable)); 
 }
 
 function onExit(event)
 {
-  air.trace("Process exited with ", event.exitCode);
+  //air.trace("Process exited with ", event.exitCode);
 }
 
 function onIOError(event)
 {
-  air.trace(event.toString());
+  //air.trace(event.toString());
 }
 
-$(document).ready(function(){
-  air.trace($("#m").length);
-  $("#m").click(function(){
-     var nativeProcessStartupInfo = new air.NativeProcessStartupInfo();
-     var file = air.File.applicationDirectory.resolvePath("vendor/jruby-1.6.0.RC2/bin/jruby");
-     nativeProcessStartupInfo.executable = file;
-
-     var processArgs = new air.Vector["<String>"]();
-     processArgs.push("--version");
-     nativeProcessStartupInfo.arguments = processArgs;
-       
-     process = new air.NativeProcess();
-     process.addEventListener(air.ProgressEvent.STANDARD_OUTPUT_DATA, dataHandler);
-    process.addEventListener(air.ProgressEvent.STANDARD_ERROR_DATA, dataHandler);
-     process.start(nativeProcessStartupInfo);
-    
-      var bytes = new air.ByteArray();
-     function dataHandler(event) {
-       process.standardOutput.readBytes(bytes, 0, process.standardOutput.bytesAvailable);
-       alert(bytes.toString());
-       air.trace(bytes.toString());
-      // $("body").append(bytes.toString());
-     }    
-  });
-});
+// $(document).ready(function(){
+//   air.trace($("#m").length);
+//   $("#m").click(function(){
+//      var nativeProcessStartupInfo = new air.NativeProcessStartupInfo();
+//      var file = air.File.applicationDirectory.resolvePath("vendor/jruby-1.6.0.RC2/bin/jruby");
+//      nativeProcessStartupInfo.executable = file;
+// 
+//      var processArgs = new air.Vector["<String>"]();
+//      processArgs.push("--version");
+//      nativeProcessStartupInfo.arguments = processArgs;
+//        
+//      process = new air.NativeProcess();
+//      process.addEventListener(air.ProgressEvent.STANDARD_OUTPUT_DATA, dataHandler);
+//     process.addEventListener(air.ProgressEvent.STANDARD_ERROR_DATA, dataHandler);
+//      process.start(nativeProcessStartupInfo);
+//     
+//       var bytes = new air.ByteArray();
+//      function dataHandler(event) {
+//        process.standardOutput.readBytes(bytes, 0, process.standardOutput.bytesAvailable);
+//        alert(bytes.toString());
+//        air.trace(bytes.toString());
+//       // $("body").append(bytes.toString());
+//      }    
+//   });
+// });
