cf304e5366e753d9deb90e2d766f3880220a41b8
Ryan Montgomery <rmontgomery429@gmail.com>
2011-02-22 11:29:02 -0500

    added shell scripts for generating an abode self signed certificate and packaging the app

---
+++ cert.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+adt -certificate -cn SelfSign -ou Dev -o "Example" -c US 2048-RSA cert.pfx password
---
+++ package.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+adt -package -storetype pkcs12 -keystore cert.pfx -target native compassapp.dmg compassapp-app.xml .
