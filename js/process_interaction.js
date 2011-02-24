ab149c05e3bb37e449ec53c3d7519723db037bd4
Ryan Montgomery <rmontgomery429@gmail.com>
2011-02-24 15:28:00 -0500

    refactoring the javascript

---
+++ js/process_interaction.js
@@ -0,0 +1,46 @@
+$(function(){
+  var process_map = {};
+  
+  $(".project").live("watch:start", startWatchingProject);
+  $(".project").live("watch:stop", stopWatchingProject);
+  air.NativeApplication.nativeApplication.addEventListener(air.Event.EXITING, killWatchingProcesses);
+  
+  function startWatchingProject(evnt, data) {
+    var nativeProcessStartupInfo = new air.NativeProcessStartupInfo();
+    nativeProcessStartupInfo.executable = air.File.applicationDirectory.resolvePath("vendor/jruby-1.6.0.RC2/bin/jruby");
+
+    var processArgs = new air.Vector["<String>"]();
+    processArgs.push("-S", "compass", "--watch", "--sass-dir", data.project.sassDir, "--css-dir", data.project.cssDir, "--environment", data.project.environment, "--output-style", data.project.outputStyle);
+    nativeProcessStartupInfo.arguments = processArgs;
+
+    process = new air.NativeProcess();
+    process.addEventListener(air.ProgressEvent.STANDARD_OUTPUT_DATA, dataHandler);
+    process.addEventListener(air.ProgressEvent.STANDARD_ERROR_DATA, dataHandler);
+    process.start(nativeProcessStartupInfo);
+
+    var bytes = new air.ByteArray();
+    function dataHandler(evnt) {
+      process.standardOutput.readBytes(bytes, 0, process.standardOutput.bytesAvailable);
+      air.trace(bytes.toString());
+    }
+
+    process_map[data.project.key] = process;
+  }
+
+  function stopWatchingProject(){
+    var project_key = $(this).attr('data-key');
+    var process = process_map[project_key];
+    if(process){
+      air.trace("Killing " + project_key);
+      process.exit();
+      delete process_map[project_key];
+    }
+  }
+  
+  function killWatchingProcesses(){
+    for (var i in process_map) {
+      process_map[i].exit();
+    }
+  }
+  
+});
