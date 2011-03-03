62fe85370de8f4da686f6b2e273e6cff65592ecf
John Hwang <jhwang@mutuallyhuman.com>
2011-03-03 12:37:30 -0600

    Added ability to select environment and output styles

--- site/js/application.js
+++ site/js/application.js
@@ -8,50 +8,50 @@ var app = {
       app[targetMethod](evnt);
     };
   },
-  
+
   initialize: function() {
     app.listProjects();
   },
-  
+
   createProjectBySelectingDirectory: function() {
     browseDirectories(air.File.userDirectory.nativePath, function(evnt) {
       if(air.Capabilities.os.match(/Windows/)) {
         app.createProject({
-          name: evnt.target.nativePath.replace(/\\$/, '').split('\\').last(), 
+          name: evnt.target.nativePath.replace(/\\$/, '').split('\\').last(),
           projectDir: evnt.target.nativePath
         });
       } else {
         app.createProject({
-          name: evnt.target.nativePath.replace(/\/$/, '').split('/').last(), 
+          name: evnt.target.nativePath.replace(/\/$/, '').split('/').last(),
           projectDir: evnt.target.nativePath
         });
       }
     });
   },
-  
-  createProjectByDroppingADirectory: function(evnt){ 
+
+  createProjectByDroppingADirectory: function(evnt){
     evnt.preventDefault();
     directoryPath = evnt.dataTransfer.getData("text/uri-list");
     app.createProject({
-      name: directoryPath.replace(/\/$/, '').split('/').last(), 
+      name: directoryPath.replace(/\/$/, '').split('/').last(),
       projectDir: directoryPath
     });
   },
-  
+
   createProject: function(options) {
-    var defaults = { 
-      name:"", 
-      projectDir:"", 
-      sassDir:"", 
-      cssDir:"", 
+    var defaults = {
+      name:"",
+      projectDir:"",
+      sassDir:"",
+      cssDir:"",
       javascriptsDir:"",
-      imagesDir:"", 
-      environment:"development", 
-      outputStyle: "expanded" 
+      imagesDir:"",
+      environment:"development",
+      outputStyle: "expanded"
     };
-    
+
     options = $.extend(defaults, options);
-    
+
     Projects.save({
       name: options.name,
       projectDir: options.projectDir,
@@ -66,7 +66,7 @@ var app = {
       $('.project[data-key='+project.key+']').trigger(':select_and_configure');
     });
   },
-  
+
   listProjects: function() {
     $('.projects').empty();
     Projects.all(function(projects) {
@@ -82,7 +82,7 @@ var app = {
       });
     });
   },
-  
+
   nukeAllProjects: function(){
     Projects.all(function(projects) {
       $.each(projects, function(i, project){
@@ -98,7 +98,7 @@ var app = {
     $('.project_details').hide();
     $('.non_selected').show();
   },
-  
+
   viewProjectConfiguration: function() {
     $('.pane.project_details').show();
     $('.pane.project_details').addClass('configure');
@@ -110,82 +110,84 @@ var app = {
     $('.pane.project_details').removeClass('configure');
     $('.pane.project_details').addClass('log');
   }
-  
+
 };
 
 // UI stuff
 $(document).ready(function() {
 
-  
+
   $.tmpl($('#colorize_template'));
   $.tmpl($('#project_template'));
   $.tmpl($('#project_details_template'));
-    
+
   // create new project
   $('.option.add').live('click', app.delegateTo('createProjectBySelectingDirectory'));
-  
+
   $('.content').live('drop', app.createProjectByDroppingADirectory);
   $('.projects').live(':changed', app.listProjects);
-  
+
   $('.project').live(':started', projectStarted);
   $('.project').live(':stopped', projectStopped);
   $('.project').live(':select', selectProject);
   $('.project').live(':select_and_configure', selectProjectConfiguration);
-  
+
   $('.project_details').live(':newLogOutput', updateProjectLog);
-  
+
   $('.modes .mode.configure').live('click', app.viewProjectConfiguration);
   $('.modes .mode.log').live('click', app.viewProjectLog);
-  
+
   // start/stop project
   $('.project .start').live('click', startWatchingProject);
   $('.project .stop').live('click', stopWatchingProject);
-  
+
   $('.select_sass_dir').live('click', selectSassDirBySelectingDirectory);
   $('.select_css_dir').live('click', selectCssDirBySelectingDirectory);
   $('.select_javascripts_dir').live('click', selectJavascriptsDirBySelectingDirectory);
   $('.select_images_dir').live('click', selectImagesDirBySelectingDirectory);
+  $('.select_environment').live('change', selectEnvironment);
+  $('.select_output_style').live('change', selectOutputStyle);
   $('.project_details .delete').live('click', deleteProject);
-  
+
   $('.project .item').live('click', function() {
     key = $(this).parents('.project:first').attr('data-key');
     $('.project_details').hide();
     $('.project_details[data-key='+key+']').show();
     $(this).parents('.project:first').trigger(':select');
   });
-  
+
   $('#nuke').live('click', app.delegateTo('nukeAllProjects'));
-  
+
   function updateProjectLog(evnt, data) {
     var key = $(this).attr('data-key');
     $('.project_details[data-key='+key+'] .log_output').append(colorize(data.replace("\n", "<br />")));
   }
-  
+
   function selectProject() {
     $('.project').removeClass('selected');
     $(this).addClass('selected');
   }
 
-  function selectProjectConfiguration(){ 
+  function selectProjectConfiguration(){
     $(this).trigger(":select");
     app.viewProjectConfiguration();
   }
-  
+
   var colors = {
     "33": "yellow",
     "32": "green",
     "31": "red",
     "0": ""
   }
-  
+
   function colorize(string) {
     new_string = string.replace(/\033\[(\d+)m([^\033]+)\033\[0m/g, function(match, color, string, offset, original) {
       thing = $.tmpl($('#colorize_template'),  { color: colors[color], string: string }).html();
       return thing;
-    });    
+    });
     return new_string.replace(/\033\[(\d+)m/g, '');
   }
-  
+
   function startWatchingProject() {
     var project_container = $(this).parents('.project:first');
     key = project_container.attr('data-key');
@@ -195,7 +197,7 @@ $(document).ready(function() {
     });
     return false;
   }
-  
+
   function stopWatchingProject(){
     var project_container = $(this).parents('.project:first');
     key = project_container.attr('data-key');
@@ -205,7 +207,7 @@ $(document).ready(function() {
     });
     return false;
   }
-  
+
   function setProjectState(project, state){
     $(project).removeClass("starting")
       .removeClass("stopping")
@@ -213,15 +215,15 @@ $(document).ready(function() {
       .removeClass("stopped")
       .addClass(state);
   };
-  
+
   function projectStopped() {
     setProjectState(this, "stopped");
   }
-  
+
   function projectStarted() {
     setProjectState(this, "started");
   }
-  
+
   app.initialize();
 });
 
@@ -233,10 +235,10 @@ function deleteProject() {
   });
   $('.project[data-key='+key+']:first').trigger('watch:stop');
   $('.projects').trigger(':changed');
-  
+
   $('.project_details').hide();
   $('.non_selected').show();
-  
+
   return false;
 }
 
@@ -288,6 +290,29 @@ function selectImagesDirBySelectingDirectory() {
   return false;
 }
 
+function selectEnvironment() {
+  var project_details = $(this).parents('.project_details:first');
+  var key = project_details.attr('data-key');
+  Projects.get(key, function(project) {
+    var environment = $(project_details).find('select.select_environment option:selected').attr('data-environment');
+    project.environment = environment;
+    Projects.save(project);
+  });
+
+  return false;
+}
+
+function selectOutputStyle() {
+  var project_details = $(this).parents('.project_details:first');
+  var key = project_details.attr('data-key');
+  Projects.get(key, function(project) {
+    var output_style = $(project_details).find('select.select_output_style option:selected').attr('data-output_style');
+    project.outputStyle = output_style;
+    Projects.save(project);
+  });
+
+  return false;
+}
 
 function browseDirectories(initialPath, callback) {
   var directory = new air.File(initialPath);
