e107fd54a5fedd1763c0a9b80e9b0674647739b6
TheJaredWilcurt <TheJaredWilcurt@users.noreply.github.com>
2015-05-19 13:13:27 -0400

    Upon this rubble I shall build
    Clean slate! No more of this JavAiRuby, we all about that NW!

--- config/compass.rb
+++
@@ -1,3 +0,0 @@
-require File.expand_path(File.join(File.dirname(__FILE__), 'environment'))
-
-additional_import_paths = [Scout.src_stylesheets_directory]
--- config/version.rb
+++
@@ -1,2 +0,0 @@
-# Adobe AIR apps cannot use alphabetic characters in their versions, so no release candidates :(
-SCOUT_VERSION = "0.7.1"
