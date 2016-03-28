70180cc35d95d6f6518c54593544c43d486622ff
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2016-03-28 08:12:02 -0400

    Load settings feature done
    Made icon auto-guesser more thorough also.

---
+++ _scripts/load-settings.js
@@ -0,0 +1,38 @@
+
+(function(){
+    var gui = require("nw.gui");
+    var fs = require("fs");
+
+    var settingsFile = "";
+
+    //If you're on windows then folders in file paths are separated with `\`, otherwise OS's use `/`
+    if ( process.platform == "win32" ) {
+        //Find the path to the settings file and store it
+        settingsFile = (gui.App.dataPath + "\\scout-settings.json");
+    } else {
+        //Find the path to the settings file and store it
+        settingsFile = (gui.App.dataPath + "/scout-settings.json");
+    }
+
+    var settingsJSON = "";
+    //Attempt to read the settings file
+    try {
+        settingsJSON = fs.readFileSync(settingsFile, {encoding: "utf-8"});
+    } catch (err) {
+        //If the file does exist grab it's error code
+        if (err.code === 'ENOENT') {
+            console.info('No settings file found, no biggie.');
+        } else {
+            throw err;
+        }
+    }
+
+    //Verify we got data back from reading the file
+    if (settingsJSON.length > 1) {
+        //Convert it from a string to JSON
+        var settingsObj = JSON.parse(settingsJSON);
+        //update the scout object
+        window.scout = settingsObj;
+    }
+
+})()
