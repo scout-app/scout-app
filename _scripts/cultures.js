9e8e9c3cd9967c4799733e4a1b954540f86d074d
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2016-04-03 08:10:53 -0400

    Update localization system

--- _scripts/cultures.js
+++ _scripts/cultures.js
@@ -17,7 +17,17 @@
         return scout.dictionary[phrase];
     }
 
+    function updateDataLangs () {
+        var items = $("*[data-lang]");
+        for (var i = 0; i < items.length; i++) {
+            var item = items[i];
+            var langKey = $(item).data("lang");
+            $(item).html( localize(langKey) );
+        }
+    }
+
     setLanguage();
+    updateDataLangs();
     scout.helpers.setLanguage = setLanguage;
     scout.localize = localize;
 
