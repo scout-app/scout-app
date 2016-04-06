5ffd09d0004a81566b5cbb25cc571dc9d07062b3
TheJaredWilcurt <TheJaredWilcurt@users.noreply.github.com>
2016-04-06 17:12:55 -0400

    Theme swapping done

--- _scripts/ugui-override.js
+++ _scripts/ugui-override.js
@@ -9,6 +9,9 @@
 (function(){
 
 
+    var fs = require('fs');
+
+
     //* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
     //### D06. Check for updates
     //
@@ -111,16 +114,77 @@
 
 
     //* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+    //### F06. Save chosen Bootswatch
+    //
+    //>In File > Preferences > Theme, when the user picks a new theme
+    // read the contents of the index.html, find the line that sets
+    // which CSS theme to use and update it to the new chosen theme.
+    // Then replace the contents of index.html with the new data so
+    // on every load it uses the correct theme.
+
+    //
+    function saveNewSwatch(newSwatch) {
+        //Validate that the required argument is passed and is the correct type
+        if (!newSwatch || typeof(newSwatch) !== "string") {
+            console.info("You must pass in a new swatch as a string");
+            return;
+        }
+
+        //Set the filename to whatever the page is NW.js opens on launch, like index.htm
+        var filename = ugui.app.startPage;
+
+        //Read the contents of index.htm like a normal file and put them in the "data" variable
+        fs.readFile(filename, "utf8", function(err, data) {
+            //If it can't read it for some reason, throw an error
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            //Set up for the regex
+            var re_start = '(<link rel="stylesheet" href="_style\\/ven\\.bootswatch\\/)';
+            var re_file = '((?:[a-z][a-z\\.\\d_]+)\\.(?:[a-z\\d]{3}))(?![\\w\\.])';
+            var re_end = '(" data-swatch="swapper">)';
+
+            //Would match: `<link rel="stylesheet" href="_style/ven.bootswatch/cerulean.min.css" data-swatch="swapper">`
+            var createRegex = RegExp(re_start + re_file + re_end, ["i"]);
+            var findSwatchLine = createRegex.exec(data);
+            //If we could find the line in the file
+            if (findSwatchLine != null) {
+                //Though not currently using this line, it may come in handy someday
+                //`var currentSwatch = findSwatchLine[52];`
+
+                var lineToFind = '<link rel="stylesheet" href="' + newSwatch + '" data-swatch="swapper">';
+
+                //Take the contents of index.htm, find the correct line, and replace that line with the updated swatch
+                data = data.replace(createRegex, lineToFind);
+            }
+
+            //With the contents of index.htm update, save over the file
+            fs.writeFile(filename, data, function(err) {
+                if (err) {
+                    console.log(err)
+                    return;
+                };
+            });
+        });
+    }
+
+
+
+
+
+
+
+    //* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
     //### F05. Swap Bootswatches
     //
-    //>This function is only ran when in dev mode. It grabs a list
-    // of all files in the `ven.bootswatch` folder and puts them in
-    // a dropdown box in UGUI Developer Toolbar so developers can
-    // try out different stylesheets.
+    //>This grabs a list of all files in the `ven.bootswatch` folder
+    // and puts them in a dropdown box under File > Preferences, so
+    // users can try out different stylesheets.
 
     //
     function themeSwapper() {
-        var fs = require('fs');
         //Grab all the files in the `ven.bootswatch` folder and put them in an array
         var allSwatches = fs.readdir("_style/ven.bootswatch", function(err, files) {
             //If that works
@@ -129,6 +193,7 @@
                 for (index = 0; index < files.length; index++) {
                     var cssFileName = files[index];                     //simplex.min.css
                     var swatchName = files[index].split(".min.css")[0]; //simplex
+                    swatchName = swatchName.split(".css")[0];           //For files without ".min" but with ".css"
                     $("#themeChoices").append(
                         '<option value="_style/ven.bootswatch/' + cssFileName + '">' +
                           swatchName +
@@ -138,21 +203,35 @@
             } else {
                 console.warn("Could not return list of style swatches.");
             }
+            //Set the correct item in the dropdown to be selected.
+            var pageSetting = $("head link[data-swatch]").attr("href");
+
+            for (var i = 0; i < $("#themeChoices option").length; i++) {
+                if ($( $("#themeChoices option")[i] ).val() == pageSetting) {
+                    $( $("#themeChoices option")[i] ).prop("selected", true);
+                }
+            }
         });
 
+
         //When you change what is selected in the dropdown box, swap out the current swatch for the new one.
-        $("#swatchSwapper").change( function() {
-            $("head link[data-swatch]").attr( "href", $("#swatchSwapper").val() );
+        $("#themeChoices").change( function() {
+            //The currently selected swatch
+            var newSwatch = $("#themeChoices").val();
+            $("head link[data-swatch]").attr("href", newSwatch);
+
             //Nav logo wasn't vertically centering after changing a stylesheet because the function was being ran after
             //the stylesheet was swapped instead of after the page rendered the styles. Since Webkit does not have a way of
             //indicating when a repaint finishes, unfortunately a delay had to be used. 71 was chosen because 14 FPS is the
             //slowest you can go in animation before something looks choppy.
             window.setTimeout(ugui.helpers.centerNavLogo, 140);
             window.setTimeout(ugui.helpers.sliderHandleColor, 140);
+
+            //Update index.htm to use the selected swatch as the new default
+            saveNewSwatch(newSwatch);
         });
     }
 
     themeSwapper();
-
 
 })();
