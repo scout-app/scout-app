5e9069e2b7a43cf60ace9bfba0a05597676f3e80
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2016-07-09 18:42:52 -0400

    update build, not done yet

--- build.js
+++ build.js
@@ -19,29 +19,32 @@ var s = 'scout-files/';
 // Clean build folder
 console.log('cleaning build folder');
 if (fs.existsSync(o + 'License'))          { fs.removeSync(o + 'License')          }
-if (fs.existsSync(o + 'scout-files'))      { fs.removeSync(o + 'scout-files')      }
 if (fs.existsSync(o + 'bower_components')) { fs.removeSync(o + 'bower_components') }
 if (fs.existsSync(o + 'node_modules'))     { fs.removeSync(o + 'node_modules')     }
+if (fs.existsSync(o + s))                  { fs.removeSync(o + s)                  }
+fs.mkdirsSync(o + s);
 
 // Copy files over
 console.log('copying files');
-fs.copySync('package.json', o + 'package.json');
-fs.copySync('bower.json', o + 'bower.json');
-fs.mkdirsSync(o + s);
+fs.copySync('package.json',   o + 'package.json');
+fs.copySync('bower.json',     o + 'bower.json');
 fs.copySync(s + 'index.html', o + s + 'index.html');
 
 // Copy folders over
 console.log('copying folders');
-fs.copySync('License', o + 'License');
+fs.copySync('License',        o + 'License');
 fs.copySync(s + '_fonts',     o + s + '_fonts');
 fs.copySync(s + '_img',       o + s + '_img');
 fs.copySync(s + '_markup',    o + s + '_markup');
 fs.copySync(s + '_scripts',   o + s + '_scripts');
-//issue here
-fs.copySync(s + '_styles',    o + s + '_styles');
+fs.copySync(s + '_style',     o + s + '_style');
 fs.copySync(s + '_themes',    o + s + '_themes');
 fs.copySync(s + 'mixins',     o + s + 'mixins');
 fs.copySync(s + 'cultures',   o + s + 'cultures');
 fs.removeSync(o + s + 'cultures/cultures.xls');
 fs.removeSync(o + s + 'cultures/README.md');
+
+// Run executables
+var exec = require("child_process").execSync;
+exec('npm --prefix ../scout-app-build install --production ../scout-app-build');
 
